714e340c72ca968eb45de4e1139460e228d46cbe
cvasquezn <christian.vasquez.n@gmail.com>
2019-04-26 20:02:00 -0300

    Updates

---
+++ precache-manifest.407367f426f041b00db04c066c0784a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a73abdcfa4072db5abd",
+    "url": "/facerecognition/static/js/runtime~main.1a73abdc.js"
+  },
+  {
+    "revision": "720a59cec9488f3a2d17",
+    "url": "/facerecognition/static/js/main.720a59ce.chunk.js"
+  },
+  {
+    "revision": "c26d11f174f2e68f6555",
+    "url": "/facerecognition/static/js/1.c26d11f1.chunk.js"
+  },
+  {
+    "revision": "720a59cec9488f3a2d17",
+    "url": "/facerecognition/static/css/main.458793f3.chunk.css"
+  },
+  {
+    "revision": "c26d11f174f2e68f6555",
+    "url": "/facerecognition/static/css/1.3b203025.chunk.css"
+  },
+  {
+    "revision": "66da4033eb48b1afc5ef06e7ae211f54",
+    "url": "/facerecognition/index.html"
+  }
+];
